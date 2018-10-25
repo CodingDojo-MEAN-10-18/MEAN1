01d9799f4b1fd1ab73b7d811dc2cbe3e09b5fd25
dojo-json <jfranz@codingdojo.com>
2018-10-24 17:03:40 -0700

    express middleware

--- week3/day1/server.js
+++ week3/day1/server.js
@@ -5,12 +5,24 @@ const path = require('path');
 const port = process.env.PORT || 8000;
 const app = express();
 
+const logger = require('./server/middleware/logger');
+
+console.log(logger);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(parser.urlencoded({ extended: true }));
+app.use(logger);
+app.use(function (request, response, next) {
+  console.log('next middleware');
+  const error = new Error('i am an error');
+  next(error);
+});
 
 const names = ['Bob', 'Nayyer', 'Jeremy'];
+
+
 
 app.get('/', function (request, response) {
 
@@ -24,9 +36,9 @@ app.post('/names', function (request, response) {
 
   names.push(request.body.name);
 
-  // response.render('names', { name: request.body.name, names });
+  response.render('names', { name: request.body.name, names });
 
-  response.redirect('/');
+  // response.redirect('/');
 });
 
 app.get('/names/:name_id', function (request, response) {
@@ -35,5 +47,17 @@ app.get('/names/:name_id', function (request, response) {
   response.send(names[request.params.name_id]);
 });
 
+
+app.use(function (error, request, response, next) {
+  // logging to db;
+  console.log(error.message);
+
+  next(error);
+});
+
+
+app.use(function (error, request, response, next) {
+  response.send(error.message);
+});
 
 app.listen(port, () => console.log(`Express server listening on port ${port}`));
